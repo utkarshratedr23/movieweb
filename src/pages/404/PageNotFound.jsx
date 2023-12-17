90115a003824aa632376a8088f1b72372d5b0fdb
utkarsh bhargava <bhargavautkarsh074@gmail.com>
2023-12-17 23:15:14 +0530

    error fixed

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import './style.scss'
-const PageNotFound = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import React from "react";
 
-export default PageNotFound
+import "./style.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
