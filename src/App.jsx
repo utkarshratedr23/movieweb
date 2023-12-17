6b90b4f19c469c55ac84281bd0d1725b67467312
utkarsh bhargava <bhargavautkarsh074@gmail.com>
2023-12-17 23:28:28 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useSelector,useDispatch } from "react-redux";
 import { fetchDataFromApi } from "./utils/api";
 import { getApiConfiguration,getGenres } from "./store/homeSlice";
 import Header from "./components/header/Header";
-import Footer from "./components/footer/footer";
+import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import Explore from "./pages/explore/Explore";
